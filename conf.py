4aa48b79db53e142ee1af58e67c8d7455171341f
Armin Töpfer <atoepfer@pacificbiosciences.com>
2022-02-02 20:05:00 +0100

    Copyright 2022

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PacBioFileFormats'
-copyright = u'2015, Pacific Biosciences'
+copyright = u'2015-2022, Pacific Biosciences'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
